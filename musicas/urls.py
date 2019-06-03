12489f672bd03c4b0c0fa6524a0d78accaec3ea2
RICARDO FERREIRA <51332428+Elvolox@users.noreply.github.com>
2019-06-03 18:51:13 -0300

    Atualização de projeto
    Projeto em andamento, futuro APP de musicas

--- musicas/urls.py
+++ musicas/urls.py
@@ -1,2 +1,11 @@
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+from . import views
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+]
--- musicas/views.py
+++ musicas/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Album
+from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+
+def index(request):
+    all_albums = Album.objects.all()
+    return render(request, 'musicas/index.html', {'all_albums': all_albums})
+
+
+def detail(request, album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Esse album não foi encontrado no banco de dados")
+    return render(request, "musicas/detail.html", {'album': album})
+
